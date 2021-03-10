a0abba88f1dc91aa09a2001cba49bbb4526e9132
Joshua Gandolfo <joshua.gandolfo@mduresources.com>
2021-03-09 23:53:52 -0700

    fix row height check desc
     cursor.setHeight(Math.max(cursor.getHeight(),glyph.getBounds().getHeight()));

--- Homework1/src/main/java/Row.java
+++ Homework1/src/main/java/Row.java
@@ -4,9 +4,16 @@ public class Row extends Composition {
 
     public Row(Compositor compositor) {
         super(compositor);
-//        setBounds(new Bounds(new Point(0, 0), 0, 0));
-        ;
     }
+    public Row(String string, Compositor compositor) {
+         super(compositor);
+         try {
+             for (int i=0; i<string.length(); i++)
+                 insert(new Character(string.charAt(i)), i);
+             } catch (Exception e) {
+             System.out.println(e);
+             }
+        }
 
     @Override
     void draw(Window window) {
@@ -25,13 +32,12 @@ public class Row extends Composition {
 
     public void updateCursor(Bounds cursor, Glyph glyph) {
     cursor.setX(glyph.getBounds().getWidth()+cursor.getPoint().x);
-     cursor.setHeight(Math.max(cursor.getHeight(),Math.max(cursor.getPoint().y,glyph.getBounds().getHeight())));
+     cursor.setHeight(Math.max(cursor.getHeight(),glyph.getBounds().getHeight()));
      cursor.setWidth(cursor.getWidth()+glyph.getBounds().getWidth());
-//        cursor.setY(cursor.getPoint().y);
+
     }
 
     public void adjustParent(Bounds bounds){
-//getBounds().setX(bounds.getPoint().x)
         getBounds().setHeight(bounds.getHeight());
         getBounds().setWidth(bounds.getWidth());
     }
