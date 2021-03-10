902630c323bb78226e3b044d347a2ebaa7dd7780
Joshua Gandolfo <joshua.gandolfo@mduresources.com>
2021-03-09 23:13:34 -0700

    HW2 Start - added both swing and window for real this time

--- Homework1/src/main/java/Window.java
+++ Homework1/src/main/java/Window.java
@@ -6,9 +6,10 @@ public interface Window {
 
     int charWidth(char c);
     int charHeight(char c);
-    int getHeight();
-    int getWidth();
 
     void setContents(Glyph glyph);
+
+    void addBorder(int x1, int y1, int x2, int y2, int width);
+    void addScrollBar(int x, int y, int width, int height);
 
 }
